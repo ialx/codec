c460dce796314ed4461443bff458298a840af8cd
Alexander V. Ignatyev <ialx@is74.ru>
2019-09-01 00:43:48 +0500

    fixed CODEC_FLAG_GLOBAL_HEADER undeclared error

--- h264enc.go
+++ h264enc.go
@@ -33,7 +33,7 @@ import (
 		m->ctx->height = m->w;
 		m->ctx->bit_rate = m->bitrate;
 		m->ctx->pix_fmt = m->pixfmt;
-		m->ctx->flags |= CODEC_FLAG_GLOBAL_HEADER;
+		m->ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
 		m->f = av_frame_alloc();
 		return avcodec_open2(m->ctx, m->c, NULL);
 	}
